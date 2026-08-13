ae5c9854468c0e68ddd72e4bf6b7478af6cb4d1c
frankcontact89-sketch <frankcontact89@gmail.com>
2026-08-13 13:27:59 -0400

    Keep current Community backend route until access database is aligned

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/IndexWithCommunity";
 import ResetPassword from "./pages/ResetPassword";
 import OAuthConsent from "./pages/OAuthConsent";
-import Community from "./pages/CommunityAccess";
+import Community from "./pages/CommunityV2";
 
 const queryClient = new QueryClient();
 
