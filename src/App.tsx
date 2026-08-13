49679503dcaf9252d9136ff186d55213397551ed
frankcontact89-sketch <frankcontact89@gmail.com>
2026-08-13 10:44:49 -0400

    Restore existing Community route

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ResetPassword from "./pages/ResetPassword";
 import OAuthConsent from "./pages/OAuthConsent";
-import Community from "./pages/CommunityEnhanced";
+import Community from "./pages/Community";
 
 const queryClient = new QueryClient();
 
